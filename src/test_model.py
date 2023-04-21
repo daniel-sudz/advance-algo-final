d37c62bf3b3f7ab4229f147859f2c764f9266b66
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-04-21 12:40:13 -0400

    yesterdays work

--- src/test_model.py
+++ src/test_model.py
@@ -38,13 +38,15 @@ def assert_valid_set(cards_on_board, result, num_properties, num_values):
         if(cards_in_set[0][p] == cards_in_set[1][p]):
             for c1 in range(num_values):
                 for c2 in range(num_values):
-                    assert cards_in_set[c1][p] == cards_in_set[c2][p]
+                    if c1 != c2:
+                        assert cards_in_set[c1][p] == cards_in_set[c2][p]
         # all values must be different
         else:
             print(cards_in_set)
             for c1 in range(num_values):
                 for c2 in range(num_values):
-                    assert cards_in_set[c1][p] != cards_in_set[c2][p]
+                    if c1 != c2:
+                        assert cards_in_set[c1][p] != cards_in_set[c2][p]
 
 # a (p=2, v=2) board should always have a valid set with 2 cards
 def test_small_board_match_different():
@@ -74,7 +76,7 @@ def test_small_board_match_same():
 
 # run finding a valid set on a traditional board in which we know sets exists and make sure we find one
 def test_stress_test_traditional_set():
-    iterations = 1
+    iterations = 10
     for i in range(iterations):
         cards_on_board = create_random_board_traditional_set()
         num_properties = 4
